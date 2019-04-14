a89de7fa980661cc3472f52a2d1536a8e5c7fa8a
LuisFelype <luis.moreira@estudante.ufla.br>
2019-04-14 19:34:04 -0300

    Criação classe D, criando métodos B3 e C3

---
+++ classeD.py
@@ -0,0 +1,13 @@
+class D:
+    D1=None
+    D2=None
+
+    def __init__(self):
+        D1=""
+        D2=0.0
+    
+    def MD1(self):
+        print("MD1")
+    
+    def MD2(self):
+        print("MD2")
