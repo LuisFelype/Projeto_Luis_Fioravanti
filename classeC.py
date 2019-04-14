33a99db09f442734525ee1000547d11c0575cbea
LuisFelype <luis.moreira@estudante.ufla.br>
2019-04-14 19:14:15 -0300

    Criação da classe C

---
+++ classeC.py
@@ -0,0 +1,13 @@
+class C:
+    C1=None
+    C2=None
+
+    def __init__(self):
+        C1=""
+        C2=0.0
+    
+    def MC1(self):
+        print("MC1")
+    
+    def MC2(self):
+        print("MC2")
